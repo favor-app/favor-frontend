a2dd602d069eced3f8dbf62cc87a1c1a7458df6b
Aryan Patel <aroo2001@gmail.com>
2022-05-01 21:25:35 -0700

    Finished Login Signup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
   Code,
   Grid,
@@ -23,15 +22,7 @@ function App() {
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               Learn Chakra
-            </Link>
           </VStack>
         </Grid>
       </Box>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider  theme={theme}>
+      <ColorModeScript />
+      <Signup />
+    </ChakraProvider>
   </StrictMode>,
   document.getElementById('root')
 );
@@ -16,9 +18,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
