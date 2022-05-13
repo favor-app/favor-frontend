7e2fa34e9501163c36187978db3a4a775b9f688a
Aryan Patel <aroo2001@gmail.com>
2022-05-12 20:14:15 -0700

    Added Phone Number

--- src/pages/FavorStatus.js
+++ src/pages/FavorStatus.js
@@ -12,6 +12,7 @@ import {
   theme,
   Link,
   useToast,
+  Heading,
 } from '@chakra-ui/react';
 import FavorCard from '../components/FavorCard';
 import Navbar from '../components/Navbar';
@@ -22,6 +23,7 @@ import { useState, useRef, useEffect } from 'react';
 
 const UPDATE_STATUS_URL = process.env.REACT_APP_URL + '/favors/updateStatus';
 const GET_TRADE_URL = process.env.REACT_APP_URL + '/trades/byFavorId';
+const GET_USER_ID_URL = process.env.REACT_APP_URL + '/users/byId';
 const UPDATE_COINS_URL = process.env.REACT_APP_URL + '/users/updateCoins';
 async function updateStatus(favorId, status) {
   try {
@@ -34,44 +36,60 @@ async function updateStatus(favorId, status) {
   }
 }
 
-async function getTradeDetails(favorId) {
-  try {
-    const response = await axios.get(GET_TRADE_URL, {
-      params: { favorId: favorId },
-      headers: { 'auth-token': localStorage.getItem('auth-token') },
-    });
-    const tradeData = response.data;
-    return tradeData;
-  } catch (err) {
-    console.log(err.response);
-  }
-}
-
-async function updateCoins(favorDetails, type, coins, status) {
-  try {
-    getTradeDetails(favorDetails._id).then(async tradeDetails => {
-      let userId;
-      if (status === 'Completed') {
-        userId = tradeDetails[0].favorerId;
-      } else {
-        userId = favorDetails.favoreeId;
-      }
-      const response = await axios.get(UPDATE_COINS_URL, {
-        params: { type: type, favorCoins: coins, userId: userId },
-        headers: { 'auth-token': localStorage.getItem('auth-token') },
-      });
-    });
-  } catch (err) {
-    console.log(err.response);
-  }
-}
-
 function FavorStatus() {
   const toast = useToast();
   const location = useLocation();
   const navigate = useNavigate();
   let mac_button = <> </>;
   let cancel_button = <> </>;
+  const [phoneNumber, setPhoneNumber] = useState(<></>);
+
+  async function getTradeDetails(favorId) {
+    try {
+      const response = await axios.get(GET_TRADE_URL, {
+        params: { favorId: favorId },
+        headers: { 'auth-token': localStorage.getItem('auth-token') },
+      });
+      const tradeData = response.data;
+      return tradeData;
+    } catch (err) {
+      console.log(err.response);
+    }
+  }
+
+  async function updateCoins(favorDetails, type, coins, status) {
+    try {
+      getTradeDetails(favorDetails._id).then(async tradeDetails => {
+        let userId;
+        if (status === 'Completed') {
+          userId = tradeDetails[0].favorerId;
+        } else {
+          userId = favorDetails.favoreeId;
+        }
+        const response = await axios.get(UPDATE_COINS_URL, {
+          params: { type: type, favorCoins: coins, userId: userId },
+          headers: { 'auth-token': localStorage.getItem('auth-token') },
+        });
+      });
+    } catch (err) {
+      console.log(err.response);
+    }
+  }
+
+  async function updatePhoneNumber(userId) {
+    try {
+      const response = await axios.get(GET_USER_ID_URL, {
+        params: { userId: userId },
+        headers: { 'auth-token': localStorage.getItem('auth-token') },
+      });
+      console.log(userId);
+      const phone = response.data.phone;
+      return phone;
+    } catch (err) {
+      console.err(err.response);
+    }
+  }
+
   if (location.state.isFavoree === true) {
     if (location.state.favorDetails.status === 'Accepted') {
       mac_button = (
@@ -146,6 +164,26 @@ function FavorStatus() {
     }
   }
 
+  useEffect(() => {
+    if (location.state.isFavoree === true && location.state.favorDetails.status === 'Accepted') {
+      console.log(location.state.favorDetails);
+      getTradeDetails(location.state.favorDetails._id)
+        .then(trade => {
+          return trade[0].favorerId;
+        })
+        .then(favorerId => updatePhoneNumber(favorerId))
+        .then(number =>
+          setPhoneNumber(<Heading size='lg' ml='1rem'>Favorer Number: {number}</Heading>)
+        );
+    } 
+    else if (location.state.isFavoree === false) 
+    {
+      updatePhoneNumber(location.state.favorDetails.favoreeId).then(number =>
+        setPhoneNumber(<Heading size='lg' ml='1rem'>Favoree Number: {number}</Heading>)
+      );
+    }
+  }, []);
+
   const status = location.state.favorDetails.status;
   let progressVal = 25;
   if (status === 'Accepted') {
@@ -155,6 +193,7 @@ function FavorStatus() {
   } else if (status === 'Expired') {
     progressVal = 0;
   }
+
   return (
     <Flex
       mx={'auto'}
@@ -184,6 +223,7 @@ function FavorStatus() {
         Favor {location.state.favorDetails.status}
       </Text>
       <FavorCard details={location.state.favorDetails} description={'show'} />
+      {phoneNumber}
       {mac_button}
       {cancel_button}
       <Box mt="3em">
