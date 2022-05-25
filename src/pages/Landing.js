67738fdd3b8d4134403ba7cacc708eae2d690002
Aryan Patel <aroo2001@gmail.com>
2022-05-24 23:35:53 -0700

    minor

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -22,7 +22,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function CallToActionWithVideo() {
   const navigate = useNavigate();
   return (
-    <Box bg="white">
+    <Box bgGradient='linear(to-b, white, blue.100, blue.200)'>
       <Flex
         direction={{ base: 'column', md: 'row', lg: 'row' }}
         pb="4rem"
@@ -37,26 +37,21 @@ export default function CallToActionWithVideo() {
           py="1rem"
           direction="column"
         >
-          <Image w="25rem" src={Logo} />
+          <Image w="30rem" src={Logo} />
           <Heading
             textAlign={'center'}
             fontWeight={600}
-            fontSize="5vh"
+            fontSize="2rem"
             color="#0077b6"
+            pb='2rem'
           >
-            Need help with something?
+            A crypto-based gig marketplace for the UCLA community!
           </Heading>
-            A crypto-based gig marketplace for UCLA
-          <Text textAlign={'center'} color={'gray.500'} py="1rem">
-            There are two things common to every college student: we are always
-            low on cash and we want to try out a million different things in
-            college. So we decided to kill both birds with one app while also
-            helping you make some new friends along the way.
-          </Text>
+
           <Stack mt="1rem">
             <Button
               rounded={'2xl'}
-              py="1.5rem"
+              py="2rem"
               px="5rem"
               fontWeight={'normal'}
               fontSize="1.3rem"
@@ -70,21 +65,7 @@ export default function CallToActionWithVideo() {
           </Stack>
         </Flex>
 
-        <Flex
-          justify={'center'}
-          align={'center'}
-          display={{ base: 'none', lg: 'block' }}
-        >
-          <Image
-            borderRadius={'3rem'}
-            px="2rem"
-            py="2rem"
-            w={'80rem'}
-            src={
-              'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-            }
-          />
-        </Flex>
+   
       </Flex>
       <Box p="2rem" bg={'blue.700'}>
         <Faq />
